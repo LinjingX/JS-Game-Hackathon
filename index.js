d637714902433580031c1ddc3d35da3dcea2db37
limnj22 <limnj@student.unimelb.edu.au>
2023-02-21 15:50:07 +1100

    initial commit

---
+++ index.js
@@ -0,0 +1,8 @@
+const CANVAS_WIDTH = 1024*2
+const CANVAS_HEIGHT = 576*2
+
+const canvas = document.querySelector('canvas')
+const context = canvas.getContext('2d')
+
+canvas.width = CANVAS_WIDTH
+canvas.height = CANVAS_HEIGHT
