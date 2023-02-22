363c7104dadc5e0b56fd1032d74920585e85e779
limnj22 <limnj@student.unimelb.edu.au>
2023-02-22 15:48:23 +1100

    creates players, players fall w/ gravity on spawn

--- index.js
+++ index.js
@@ -1,8 +1,76 @@
+/* CANVAS CONSTANTS */
 const CANVAS_WIDTH = 1024*2
 const CANVAS_HEIGHT = 576*2
 
+/* SPRITE CONSTANTS */
+const SPRITE_WIDTH = 50*2
+const SPRITE_HEIGHT = 150*2
+
+/* OTHER CONSTANTS */
+const GRAVITY_RATE = 0.2
+
 const canvas = document.querySelector('canvas')
-const context = canvas.getContext('2d')
+const c = canvas.getContext('2d')
 
 canvas.width = CANVAS_WIDTH
 canvas.height = CANVAS_HEIGHT
+
+c.fillRect(0, 0, canvas.width, canvas.height)
+
+// Each player is represented by a Sprite class
+class Sprite {
+    
+    constructor({position, velocity}) {
+        this.position = position
+        this.velocity = velocity
+        this.height = SPRITE_HEIGHT
+    }
+
+    draw() {
+        c.fillStyle = 'red'
+        c.fillRect(this.position.x, this.position.y, SPRITE_WIDTH, this.height)
+    }
+
+    update() {
+        this.draw()
+        // make player fall on spawn
+        this.position.y += this.velocity.y
+
+        if (this.position.y + this.height + this.velocity.y >= canvas.height) {
+            // stop player from sinking into the ground upon initial impact from spawn fall
+            this.velocity.y = 0
+        } else {
+            // otherwise apply gravity
+            this.velocity.y += GRAVITY_RATE
+        }
+    }
+}
+
+const player1 = new Sprite({position: {x: 0, y: getRandomInt(-100, 100)}, velocity: {x: 0, y: 0}})
+const player2 = new Sprite({position: {x: 1000, y: getRandomInt(-100, 100)}, velocity: {x: 0, y: 0}})
+
+player1.draw()
+console.log(player1)
+
+player2.draw()
+console.log(player2)
+
+animate()
+
+/* FUNCTIONS */
+
+function animate() {
+    window.requestAnimationFrame(animate)
+    c.fillStyle = 'black'
+    c.fillRect(0, 0, canvas.width, canvas.height)
+    player1.update()
+    player2.update()
+}
+
+/* HELPER FUNCTIONS */
+
+function getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min) + min); // The maximum is exclusive and the minimum is inclusive
+}
