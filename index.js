904d664945da874ca2146b5c232252dab61b2577
Linjing Xuan <xuanlinjing@gmail.com>
2023-02-27 07:26:29 +1100

    added attackbox to both players, players now can attack

--- index.js
+++ index.js
@@ -81,18 +81,23 @@ class Sprite {
 // Each player is represented by a Fighther class, extend Sprite class
 class Fighter {
     
-    constructor({position, velocity}) {
+    constructor({position, velocity, offset}) {
         this.position = position
         this.velocity = velocity
         this.width = 100
         this.height = SPRITE_HEIGHT
         this.lastKey
         this.attackBox = {
-            position: this.position,
+            position: {
+                x: this.position.x,
+                y: this.position.y
+            },
+            offset,
             width: 200,
             height: 100,
         }
         this.color = 'red'
+        this.isAttacking
     }
 
     draw() {
@@ -106,6 +111,8 @@ class Fighter {
 
     update() {
         this.draw()
+        this.attackBox.position.x = this.position.x + this.attackBox.offset.x
+        this.attackBox.position.y = this.position.y
         
         this.position.y += this.velocity.y
 
@@ -119,6 +126,13 @@ class Fighter {
             this.velocity.y += GRAVITY_RATE
         }
     }
+
+    attack() {
+        this.isAttacking = true
+        setTimeout(() => {
+            this.isAttacking = false
+        }, 100)
+    }
 }
 
 /* ****************************** INITIALISE ****************************** */
@@ -128,8 +142,22 @@ c.fillRect(0, 0, canvas.width, canvas.height)
 const background = new Sprite({position: {x: 0, y: 0}, imageSrc: './assets/background.png'})
 
 // Create players
-const player1 = new Fighter({position: {x: 0, y: getRandomInt(-100, 100)}, velocity: {x: 0, y: 0}})
-const player2 = new Fighter({position: {x: 1000, y: getRandomInt(-100, 100)}, velocity: {x: 0, y: 0}})
+const player1 = new Fighter({
+    position: {x: 0, y: getRandomInt(-100, 100)}, 
+    velocity: {x: 0, y: 0},
+    offset: {
+        x: 0,
+        y: 0
+    }
+})
+const player2 = new Fighter({
+    position: {x: 1000, y: getRandomInt(-100, 100)}, 
+    velocity: {x: 0, y: 0},
+    offset: {
+        x: -100,
+        y: 0
+    }
+})
 
 // Render players
 player1.draw()
@@ -142,6 +170,15 @@ console.log(player2)
 animate()
 
 /* ****************************** FUNCTIONS ****************************** */
+
+function rectangularCollision({rectangle1, rectangle2}) {
+    return (
+        rectangle1.attackBox.position.x + rectangle1.attackBox.width >= rectangle2.position.x 
+        && rectangle1.attackBox.position.x <= rectangle2.position.x + rectangle2.width 
+        && rectangle1.attackBox.position.y + rectangle1.attackBox.height >= rectangle2.position.y
+        && rectangle1.attackBox.position.y <= rectangle2.position.y + rectangle2.height
+    )
+}
 
 // Animate (render) game
 function animate() {
@@ -168,11 +205,14 @@ function animate() {
     }
 
     // detect collision
-    if (player1.attackBox.position.x + player1.attackBox.width >= player2.position.x 
-        && player1.attackBox.position.x <= player2.position.x + player2.width 
-        && player1.attackBox.position.y + player1.attackBox.height >= player2.position.y
-        && player1.attackBox.position.y <= player2.position.y + player2.height) {
+    if (rectangularCollision({rectangle1: player1, rectangle2: player2}) && player1.isAttacking) {
+        player1.isAttacking = false
         console.log('go') 
+    }
+
+    if (rectangularCollision({rectangle1: player2, rectangle2: player1}) && player2.isAttacking) {
+        player2.isAttacking = false
+        console.log('enemy attack successful') 
     }
 }
 
@@ -181,28 +221,34 @@ window.addEventListener('keydown', (event) => {
         case 'd':
             keys.d.pressed = true
             player1.lastKey = 'd'
-        break
+            break
         case 'a':
             keys.a.pressed = true
             player1.lastKey = 'a'
-        break
+            break
         case 'w':
             player1.velocity.y = -10
-        break
+            break
+        case ' ':
+            player1.attack()
+            break
     }
 
     switch (event.key) {
         case 'ArrowRight':
             keys.ArrowRight.pressed = true
             player2.lastKey = 'ArrowRight'
-        break
+            break
         case 'ArrowLeft':
             keys.ArrowLeft.pressed = true
             player2.lastKey = 'ArrowLeft'
-        break
+            break
         case 'ArrowUp':
             player2.velocity.y = -10
-        break
+            break
+        case 'ArrowDown':
+            player2.attack()
+            break
     }
 
     console.log(event.key)
